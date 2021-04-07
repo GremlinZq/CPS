96ab6aeecc83abac673e231010afd0ca59a821e3
Daniil Reva <gremzyx@bk.ru>
2021-04-07 05:28:47 +0300

    add code

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -3,17 +3,17 @@
   function scrollHorizontally(e) {
     e = window.event || e;
     const delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-    document.querySelector('.services__info').scrollLeft -= (delta * 10); // Multiplied by 10
+    document.querySelector('.services__scroll').scrollLeft -= (delta * 10); // Multiplied by 10
     e.preventDefault();
   }
-  if (document.querySelector('.services__info').addEventListener) {
+  if (document.querySelector('.services__scroll').addEventListener) {
     // IE9, Chrome, Safari, Opera
     document.querySelector('.services__info').addEventListener("mousewheel", scrollHorizontally, false);
     // Firefox
-    document.querySelector('.services__info').addEventListener("DOMMouseScroll", scrollHorizontally, false);
+    document.querySelector('.services__scroll').addEventListener("DOMMouseScroll", scrollHorizontally, false);
   } else {
     // IE 6/7/8
-    document.querySelector('.services__info').attachEvent("onmousewheel", scrollHorizontally);
+    document.querySelector('.services__scroll').attachEvent("onmousewheel", scrollHorizontally);
   }
 
 })();
